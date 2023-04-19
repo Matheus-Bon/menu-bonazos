5c4ff4755b6d482e6ffcade73eb0066031418805
Matheus Bon <matheusbon29@gmail.com>
2023-04-19 09:19:20 -0300

    Melhorias do btn categories + responsividade md

--- resources/js/Pages/Admin/Partials/CardCategories.vue
+++ resources/js/Pages/Admin/Partials/CardCategories.vue
@@ -1,6 +1,7 @@
 <script setup>
 import BoxBorder from "@/Pages/Admin/Components/UI/BoxBorder.vue";
 import SelectMenu from "../Components/SelectMenu.vue";
+import CardCategory from "./CardCategory.vue";
 </script>
 
 <template>
@@ -8,10 +9,10 @@ import SelectMenu from "../Components/SelectMenu.vue";
         <div class="bg-white p-4 dark:bg-admin-body">
             <section>
                 <div
-                    class="p-2 border border-l-8 border-indigo-500 dark:border-indigo-700 rounded-md w-8/12"
+                    class="p-2 border border-l-8 border-indigo-500 dark:border-indigo-700 rounded-md w-8/12 md:w-full"
                 >
                     <h5
-                        class="pl-1 font-thin text-base text-gray-700 dark:text-gray-400"
+                        class="pl-1 font-thin text-base md:text-sm text-gray-700 dark:text-gray-400"
                     >
                         <i class="bi bi-info pr-2"></i>
                         Aqui é o cardápio. Aqui você define os produtos que
@@ -32,7 +33,7 @@ import SelectMenu from "../Components/SelectMenu.vue";
 
                         <div>
                             <button
-                                class="bg-none text-secondary-color-dark hover:opacity-80"
+                                class="bg-none text-secondary-color-light dark:text-secondary-color-dark hover:opacity-80"
                             >
                                 <i class="bi bi-list"></i>
                                 Reordenar
@@ -40,15 +41,15 @@ import SelectMenu from "../Components/SelectMenu.vue";
                         </div>
                     </div>
 
-                    <div class="pt-4">
+                    <div class="pt-4 flex flex-col gap-20">
                         <section class="flex flex-row justify-start gap-7">
-                            <div class="w-3/12">
+                            <div class="w-3/12 md:w-5/12">
                                 <SelectMenu />
                             </div>
 
                             <div class="self-center pt-2 flex">
                                 <button
-                                    class="text-base h-9 rounded-md rounded-r-none border border-black dark:bg-admin-card dark:border-gray-800 text-indigo-500 hover:text-indigo-700 focus:ring-indigo-700 hover:mr-2"
+                                    class="text-base h-9 rounded-md rounded-r-none border border-gray-300 dark:bg-admin-card dark:border-gray-800 text-indigo-500 hover:text-indigo-700 focus:ring-indigo-700 hover:mr-2 hover:transition-all hover:duration-300 hover:ease-in-out"
                                 >
                                     <i class="bi bi-search px-3"></i>
                                 </button>
@@ -56,9 +57,12 @@ import SelectMenu from "../Components/SelectMenu.vue";
                                 <input
                                     type="text"
                                     placeholder="Buscar Categorias"
-                                    class="p-1.5 h-9 text-gray-200 rounded-md rounded-l-none border dark:bg-admin-card dark:border-gray-800 focus:ring-2 focus:ring-indigo-700"
+                                    class="p-1.5 h-9 text-gray-200 rounded-md rounded-l-none border border-gray-300 shadow-sm dark:bg-admin-card dark:border-gray-800 focus:ring-1 focus:ring-indigo-700 md:w-4/6 md:text-sm"
                                 />
                             </div>
+                        </section>
+                        <section>
+                            <CardCategory />
                         </section>
                     </div>
                 </div>
---
+++ resources/js/Pages/Admin/Partials/CardCategory.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+    <h1 class="text-gray-100">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit neque laudantium, aliquam aspernatur debitis corporis porro quod dolore veniam quisquam ipsum voluptatibus deleniti nemo consequuntur fuga ut tempora maiores quam.
+    </h1>
+</template>
